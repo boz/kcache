5768ea082fda9c10cfca563a632c111a061a17d0
Adam Bozanich <adam.boz@gmail.com>
2017-07-18 00:34:59 -0700

    filters: test, name changes.

--- filter/filter.go
+++ filter/filter.go
@@ -10,6 +10,8 @@ import (
 )
 
 type Filter interface {
+
+	// Accept() should return true if the given object passes the filter.
 	Accept(metav1.Object) bool
 }
 
@@ -18,6 +20,7 @@ type ComparableFilter interface {
 	Equals(Filter) bool
 }
 
+// Null() returns a filter whose Accept() is always true.
 func Null() ComparableFilter {
 	return nullFilter{}
 }
@@ -33,79 +36,121 @@ func (nullFilter) Equals(other Filter) bool {
 	return ok
 }
 
-func LabelsSelector(match map[string]string) ComparableFilter {
-	return &labelsSelector{labels.Set(match)}
+type allFilter struct{}
+
+// All() returns a filter whose Accept() is always false.
+func All() ComparableFilter {
+	return allFilter{}
 }
 
-type labelsSelector struct {
-	filter labels.Set
+func (allFilter) Accept(_ metav1.Object) bool {
+	return false
 }
 
-func (f *labelsSelector) Accept(obj metav1.Object) bool {
-	if len(f.filter) == 0 {
-		return false
+func (allFilter) Equals(other Filter) bool {
+	_, ok := other.(allFilter)
+	return ok
+}
+
+// Labels() returns a filter which returns true if
+// the provided map is a subset of the object's labels.
+func Labels(match map[string]string) ComparableFilter {
+	return &labelsFilter{match}
+}
+
+type labelsFilter struct {
+	target map[string]string
+}
+
+func (f *labelsFilter) Accept(obj metav1.Object) bool {
+	if len(f.target) == 0 {
+		return true
 	}
-	return labels.AreLabelsInWhiteList(f.filter, labels.Set(obj.GetLabels()))
+
+	current := obj.GetLabels()
+
+	for k, v := range f.target {
+		if val, ok := current[k]; !ok || val != v {
+			return false
+		}
+	}
+	return true
 }
 
-func (f *labelsSelector) Equals(other Filter) bool {
-	if other, ok := other.(*labelsSelector); ok {
-		return labels.Equals(f.filter, other.filter)
+func (f *labelsFilter) Equals(other Filter) bool {
+	if other, ok := other.(*labelsFilter); ok {
+		if len(f.target) != len(other.target) {
+			return false
+		}
+		if len(f.target) == 0 {
+			return true
+		}
+		for k, v := range f.target {
+			if val, ok := other.target[k]; !ok || val != v {
+				return false
+			}
+		}
+		return true
 	}
 	return false
 }
 
-func ServiceSelector(target map[string]string) ComparableFilter {
-	return &serviceSelector{labels.Set(target)}
+// ServiceFor() returns a filter if the object
+// is a Service whose selector mateches the given target.
+func ServiceFor(target map[string]string) ComparableFilter {
+	return &serviceForFilter{target}
 }
 
-type serviceSelector struct {
-	target labels.Set
+type serviceForFilter struct {
+	target map[string]string
 }
 
-func (f *serviceSelector) Accept(obj metav1.Object) bool {
+func (f *serviceForFilter) Accept(obj metav1.Object) bool {
 	svc, ok := obj.(*v1.Service)
 
 	if !ok {
 		return false
 	}
 
-	if len(svc.Spec.Selector) == 0 {
+	if len(svc.Spec.Selector) == 0 || len(f.target) == 0 {
 		return false
 	}
 
 	for k, v := range svc.Spec.Selector {
-		if f.target.Get(k) != v {
+		if val, ok := f.target[k]; !ok || val != v {
 			return false
 		}
 	}
 
-	return false
+	return true
 }
 
-func (f *serviceSelector) Equals(other Filter) bool {
-	if other, ok := other.(*serviceSelector); ok {
+func (f *serviceForFilter) Equals(other Filter) bool {
+	if other, ok := other.(*serviceForFilter); ok {
 		return labels.Equals(f.target, other.target)
 	}
 	return false
 }
 
-func NSNamesSelector(ids ...nsname.NSName) ComparableFilter {
+// NSName() returns a filter whose Accept() returns true
+// if the object's namespace and name matches one of the given
+// NSNames.
+func NSName(ids ...nsname.NSName) ComparableFilter {
 	set := make(map[nsname.NSName]bool)
 	for _, id := range ids {
 		set[id] = true
 	}
-	return nsNameSelector(set)
+	return nsNameFilter(set)
 }
 
-type nsNameSelector map[nsname.NSName]bool
+type nsNameFilter map[nsname.NSName]bool
 
-func (f nsNameSelector) Accept(obj metav1.Object) bool {
+func (f nsNameFilter) Accept(obj metav1.Object) bool {
 	key := nsname.ForObject(obj)
 	_, ok := f[key]
 	return ok
 }
 
-func (f nsNameSelector) Equals(other Filter) bool {
+func (f nsNameFilter) Equals(other Filter) bool {
 	return reflect.DeepEqual(f, other)
 }
---
+++ filter/filter_test.go
@@ -0,0 +1,144 @@
+package filter_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/pkg/api/v1"
+
+	"github.com/boz/kcache/filter"
+	"github.com/boz/kcache/nsname"
+)
+
+type filterTests []struct {
+	object metav1.Object
+	result bool
+}
+
+func TestNullFilter(t *testing.T) {
+	f := filter.Null()
+
+	assert.True(t, f.Accept(&v1.Pod{}))
+	assert.True(t, f.Accept(&v1.Service{}))
+	assert.True(t, f.Accept(&v1.Secret{}))
+
+	assert.True(t, f.Equals(filter.Null()))
+	assert.False(t, f.Equals(nil))
+	assert.False(t, f.Equals(filter.All()))
+}
+
+func TestAllFilter(t *testing.T) {
+	f := filter.All()
+
+	assert.False(t, f.Accept(&v1.Pod{}))
+	assert.False(t, f.Accept(&v1.Service{}))
+	assert.False(t, f.Accept(&v1.Secret{}))
+
+	assert.True(t, f.Equals(filter.All()))
+	assert.False(t, f.Equals(nil))
+	assert.False(t, f.Equals(filter.Null()))
+}
+
+func TestLabels(t *testing.T) {
+	target := map[string]string{"a": "1"}
+	tsuper := map[string]string{"a": "1", "b": "2"}
+	tmiss := map[string]string{"a": "2"}
+
+	f := filter.Labels(target)
+	fempty := filter.Labels(nil)
+
+	gen := func(labels map[string]string) metav1.Object {
+		return &v1.Pod{ObjectMeta: metav1.ObjectMeta{Labels: labels}}
+	}
+
+	assert.True(t, f.Accept(gen(target)))
+	assert.True(t, f.Accept(gen(tsuper)))
+	assert.False(t, f.Accept(gen(tmiss)))
+
+	assert.True(t, fempty.Accept(gen(target)))
+	assert.True(t, fempty.Accept(gen(tsuper)))
+	assert.True(t, fempty.Accept(gen(tmiss)))
+
+	assert.True(t, f.Equals(f))
+	assert.False(t, f.Equals(fempty))
+
+	assert.True(t, fempty.Equals(fempty))
+	assert.False(t, fempty.Equals(f))
+}
+
+func TestServiceFor(t *testing.T) {
+	target := map[string]string{"a": "1"}
+	tsuper := map[string]string{"a": "1", "b": "2"}
+	tmiss := map[string]string{"a": "2"}
+
+	gensvc := func(labels map[string]string) metav1.Object {
+		return &v1.Service{Spec: v1.ServiceSpec{Selector: labels}}
+	}
+
+	{
+		f := filter.ServiceFor(target)
+		assert.True(t, f.Accept(gensvc(target)))
+		assert.False(t, f.Accept(gensvc(tsuper)))
+		assert.False(t, f.Accept(gensvc(tmiss)))
+		assert.False(t, f.Accept(gensvc(nil)))
+
+		assert.False(t, f.Accept(&v1.Pod{}))
+	}
+
+	{
+		f := filter.ServiceFor(tsuper)
+		assert.True(t, f.Accept(gensvc(target)))
+		assert.True(t, f.Accept(gensvc(tsuper)))
+		assert.False(t, f.Accept(gensvc(tmiss)))
+	}
+
+	{
+		f := filter.ServiceFor(nil)
+		assert.False(t, f.Accept(gensvc(target)))
+	}
+
+	{
+		f := filter.ServiceFor(target)
+		fsuper := filter.ServiceFor(tsuper)
+		fnil := filter.ServiceFor(nil)
+
+		assert.True(t, f.Equals(f))
+		assert.False(t, f.Equals(fsuper))
+		assert.False(t, f.Equals(fnil))
+
+		assert.True(t, fnil.Equals(fnil))
+		assert.False(t, fnil.Equals(fsuper))
+		assert.False(t, fnil.Equals(f))
+
+		assert.True(t, fsuper.Equals(fsuper))
+		assert.False(t, fsuper.Equals(fnil))
+		assert.False(t, fsuper.Equals(f))
+	}
+}
+
+func TestNSName(t *testing.T) {
+	n1 := nsname.New("a", "1")
+	n2 := nsname.New("a", "2")
+	n3 := nsname.New("b", "2")
+
+	o1 := metav1.ObjectMeta{Namespace: n1.Namespace, Name: n1.Name}
+	o2 := metav1.ObjectMeta{Namespace: n2.Namespace, Name: n2.Name}
+	o3 := metav1.ObjectMeta{Namespace: n3.Namespace, Name: n3.Name}
+
+	assert.True(t, filter.NSName(n1).Accept(&v1.Pod{ObjectMeta: o1}))
+	assert.False(t, filter.NSName(n1).Accept(&v1.Pod{ObjectMeta: o2}))
+	assert.False(t, filter.NSName(n1).Accept(&v1.Pod{ObjectMeta: o3}))
+
+	assert.True(t, filter.NSName(n1, n2).Accept(&v1.Service{ObjectMeta: o1}))
+	assert.True(t, filter.NSName(n1, n2).Accept(&v1.Service{ObjectMeta: o2}))
+	assert.False(t, filter.NSName(n1, n2).Accept(&v1.Service{ObjectMeta: o3}))
+
+	assert.False(t, filter.NSName().Accept(&v1.Service{ObjectMeta: o1}))
+
+	assert.True(t, filter.NSName().Equals(filter.NSName()))
+	assert.True(t, filter.NSName(n1).Equals(filter.NSName(n1)))
+	assert.False(t, filter.NSName(n1).Equals(filter.NSName(n2)))
+	assert.False(t, filter.NSName(n1).Equals(nil))
+	assert.False(t, filter.NSName().Equals(nil))
+}
