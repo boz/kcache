3488c40446b6cb3b2053276603eed43f886e6290
Adam Bozanich <adam.boz@gmail.com>
2017-08-14 16:21:27 -0700

    util: default kube client

---
+++ util/kclient.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func KubeClient() (kubernetes.Interface, *rest.Config, error) {
+	config, err := KubeConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientset, config, nil
+}
+
+func KubeConfig() (*rest.Config, error) {
+	config, err := rest.InClusterConfig()
+	if err == nil {
+		return config, err
+	}
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	).ClientConfig()
+}
